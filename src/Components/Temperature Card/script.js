743222a272f943a6aa08c81afa6fc359eb3dd624
Soumyo78 <soumyo78@yahoo.com>
2021-01-18 11:56:09 +0530

    Minor bugs fixed

--- src/Components/Temperature Card/script.js
+++ src/Components/Temperature Card/script.js
@@ -17,17 +17,15 @@ class ConverterCard extends React.Component{
                 inputValue: e.target.value
             },
             ()=>{
-                if (e.target.id == 'celsius-input'){
+                if (e.target.id === 'celsius-input'){
                     let result = (((9/5)*parseFloat(this.state.inputValue))+32);
                     document.getElementById('fahrenheit-input').value = result;
                 }
 
-                if (e.target.id == 'fahrenheit-input'){
+                if (e.target.id === 'fahrenheit-input'){
                     let result = ((5/9)*(parseFloat(this.state.inputValue)-32));
                     document.getElementById('celsius-input').value = result;
                 }
-                
-                // document.getElementById('celsius-input').value = this.state.inputValue;
             }
         )
     }
